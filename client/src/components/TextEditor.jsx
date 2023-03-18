1131839ef56a0669d69860679605ae144b0f56e0
Victorpraiz <ivictorpraiz@gmail.com>
2023-03-19 00:32:08 +0100

    configured printing styles for pages

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,15 +1,34 @@
 import React, { useCallback, useEffect, useRef } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
-import '../styles.css'
+import "../styles.css";
+
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
 
 export default function TextEditor() {
-  
   const wrapperRef = useCallback((wrapper) => {
-      if (wrapper === null) return;
-      wrapper.innerHTML = "";
-      const editor = document.createElement("div");
-      wrapper.append(editor);
+    if (wrapper === null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
     new Quill(editor, { theme: "snow" });
   }, []);
   return <div className="container" ref={wrapperRef}></div>;
